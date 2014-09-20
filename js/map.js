be8fe91c6bda6345f638660910904682952e8b97
Kei Kreutler <kei@ourmachine.net>
2014-09-20 15:17:50 +0200

    Adding points to Firebase DB

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZ2FtZXJhIiwiYSI6IjNlclVnZDAifQ.a8PjkEfE5i2aOShPawCy1A';
-var map = L.mapbox.map('map', 'keikreutler.ji79a15n');
+var map = L.mapbox.map('map', 'keikreutler.ji79a15n').
+  setView([40.68988017892701, 16.524295806884766], 5);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
-
+var base = new Firebase('https://house-starter.firebaseio.com/');
 var marker = L.marker([40.68988017892701, 16.524295806884766], {
     draggable: true,
     icon: L.mapbox.marker.icon({
@@ -13,6 +13,31 @@ var marker = L.marker([40.68988017892701, 16.524295806884766], {
     // Add a form to that marker that lets them specify a message and click Add
     // to add the data.
 })
-    .bindPopup('Click or drag to add location')
-    .openPopup()
-    .addTo(map);
+    .addTo(map)
+    .bindPopup('Click or drag to add location');
+
+marker.openPopup();
+
+marker.on('dragend', function(e) {
+    marker.openPopup();
+});
+
+function onLocation() {
+  // First, clean the potential-HTML they've added to the value.
+  var category = L.DomUtil.get('category').value;
+  // Get the current draggable marker's position and GeoJSON representation
+  var geojson = marker.toGeoJSON();
+  geojson.properties['category'] = category;
+  // geojson.properties['marker-color'] = colors[category];
+  // geojson.properties['marker-symbol'] = icons[category];
+  geojson.properties['name'] = L.DomUtil.get('name').value;
+  geojson.properties['description'] = L.DomUtil.get('description').value;
+  // And save it to Firebase
+  base.push(geojson);
+  // marker.closePopup();
+  document.getElementById('name').value='';
+  document.getElementById('category').value='0';
+  document.getElementById('description').value='';
+  // Reset the original marker
+  marker.setLatLng([40.68988017892701, 16.524295806884766]);
+};
